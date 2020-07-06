fdd509a04d2b867de432b8fbb7ea0ba728659a65
danilo-dellorco <56266784+danilo-dellorco@users.noreply.github.com>
2020-07-06 22:50:22 +0200

    first commit

---
+++ src/logic/IntegerMethods.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class IntegerMethods {
+	public String convertString(int num) {
+		return Integer.toString(num);
+	}
+	
+	public int getMax(int num1,int num2) {
+		return Integer.max(num1, num2);
+	}
+}
---
+++ src/logic/Main.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class Main {
+	public static void main (String[] args) {
+		IntegerMethods m = new IntegerMethods();
+		System.out.println(m.convertString(5));
+		System.out.println(m.getMax(5, 10));
+	}
+}
