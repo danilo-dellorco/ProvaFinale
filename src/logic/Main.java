ffc64be9c0b5a0cfec21fc8b52a4e9c491e2a08a
danilo-dellorco <56266784+danilo-dellorco@users.noreply.github.com>
2020-07-06 23:00:56 +0200

    Rimossi Code Smell

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,9 +1,12 @@
 package logic;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 	public static void main (String[] args) {
 		IntegerMethods m = new IntegerMethods();
-		System.out.println(m.convertString(5));
-		System.out.println(m.getMax(5, 10));
+		Logger.getGlobal().log(Level.INFO,m.convertString(5));
+		Logger.getGlobal().log(Level.INFO,m.convertString(m.getMax(10,20)));
 	}
 }
